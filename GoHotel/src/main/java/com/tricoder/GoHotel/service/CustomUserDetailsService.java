d74c348f4a979f577b807f026e5947eee777d8d8
TruongNgocTri12345 <truongngoctri12345@gmail.com>
2025-06-30 15:51:10 +0700

    add service

---
+++ GoHotel/src/main/java/com/tricoder/GoHotel/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.tricoder.GoHotel.service;
+
+import com.tricoder.GoHotel.exception.OurException;
+import com.tricoder.GoHotel.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Load user by username (email)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new OurException("Username/Email not Found"));
+    }
+}
