dc7fbda8fe8d9e6d7ea490c305a30ec04c465a5e
TruongNgocTri12345 <truongngoctri12345@gmail.com>
2025-07-02 23:06:54 +0700

    add all compinent from fe

--- gohotel-react/src/component/profile/EditProfilePage.jsx
+++ gohotel-react/src/component/profile/EditProfilePage.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ApiService from '../../service/ApiService';
+import { useTranslation } from "react-i18next";
 
 const EditProfilePage = () => {
     const [user, setUser] = useState(null);
     const [error, setError] = useState(null);
     const navigate = useNavigate();
+
+    const { t, i18n } = useTranslation();
 
     useEffect(() => {
         const fetchUserProfile = async () => {
@@ -34,14 +37,14 @@ const EditProfilePage = () => {
 
     return (
         <div className="edit-profile-page">
-            <h2>Edit Profile</h2>
+            <h2>{t('editProfile.title')}</h2>
             {error && <p className="error-message">{error}</p>}
             {user && (
                 <div className="profile-details">
-                    <p><strong>Name:</strong> {user.name}</p>
-                    <p><strong>Email:</strong> {user.email}</p>
-                    <p><strong>Phone Number:</strong> {user.phoneNumber}</p>
-                    <button className="delete-profile-button" onClick={handleDeleteProfile}>Delete Profile</button>
+                    <p><strong>{t('editProfile.name')}:</strong> {user.name}</p>
+                    <p><strong>{t('editProfile.email')}:</strong> {user.email}</p>
+                    <p><strong>{t('editProfile.phone')}:</strong> {user.phoneNumber}</p>
+                    <button className="delete-profile-button" onClick={handleDeleteProfile}>{t('editProfile.deleteProfile')}</button>
                 </div>
             )}
         </div>
